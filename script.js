81c38f94fa271e58b3e93e8e85796fcdb119d5a7
OrhanMA <orhan.madi.assani@gmail.com>
2023-02-28 22:01:02 +0100

    Making the first responsive version

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 "use strict";
 
-const menuIcon = document.querySelector(".menu-icon");
+const menuIcon = document.querySelector(".dropdown");
+const navLinks = document.querySelector(".header-nav");
+navLinks.style.display = "none";
 menuIcon.addEventListener("click", () => {
   const navLinks = document.querySelector(".header-nav");
   console.log("clicked");
   if (navLinks.style.display === "none") {
     navLinks.style.display = "block";
+    navLinks.style.transition = "all 2s ease";
   } else {
     navLinks.style.display = "none";
   }
