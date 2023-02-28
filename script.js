07936493b8561d8004b9ec529b2c0568e377e761
OrhanMA <orhan.madi.assani@gmail.com>
2023-02-28 23:30:06 +0100

    new hover effects; infos in README; external links on buttons

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ menuIcon.addEventListener("click", () => {
     navLinks.style.display = "none";
   }
 });
+
+const surprise1 = document.querySelector(".surprise-1");
+surprise1.addEventListener("click", () => {
+  window.open("https://motherfuckingwebsite.com/", "_blank").focus();
+});
+
+const surprise2 = document.querySelector(".surprise-2");
+surprise2.addEventListener("click", () => {
+  window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank").focus();
+});
+
+const retourTop = document.querySelector(".header-logo");
+retourTop.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
