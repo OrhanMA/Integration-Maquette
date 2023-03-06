b9b7dad26f6fa785cc42706057fc841a8c4df435
OrhanMA <orhan.madi.assani@gmail.com>
2023-03-06 14:32:49 +0100

    in the gallery section: added filter functionality for images

--- script.js
+++ script.js
@@ -76,3 +76,88 @@ if (window.innerWidth <= 385) {
   copyright.textContent = " Copyright © 2017 Your Company Design: templatemo";
   footerTinker.appendChild(copyright);
 }
+
+//fonction pour l'affichage de la gallerie
+
+// sélection dans le dom de tous les bouttons de la galerie
+const showAllButton = document.querySelector("#show-all-button");
+const brandingButton = document.querySelector("#branding");
+const graphicButton = document.querySelector("#graphic");
+const natureButton = document.querySelector("#nature");
+const animationButton = document.querySelector("#animation");
+
+// sélection dans le dom de toutes les images de la galerie
+const pic1 = document.querySelector("#pic1");
+const pic2 = document.querySelector("#pic2");
+const pic3 = document.querySelector("#pic3");
+const pic4 = document.querySelector("#pic4");
+const pic5 = document.querySelector("#pic5");
+const pic6 = document.querySelector("#pic6");
+
+showAllButton.addEventListener("click", () => {
+  showAllButton.classList.add("yellow-letter");
+  brandingButton.classList.remove("yellow-letter");
+  graphicButton.classList.remove("yellow-letter");
+  pic1.style.display = "block";
+  pic2.style.display = "block";
+  pic3.style.display = "block";
+  pic4.style.display = "block";
+  pic5.style.display = "block";
+  pic6.style.display = "block";
+});
+
+brandingButton.addEventListener("click", () => {
+  brandingButton.classList.add("yellow-letter");
+  showAllButton.classList.remove("yellow-letter");
+  graphicButton.classList.remove("yellow-letter");
+  natureButton.classList.remove("yellow-letter");
+  animationButton.classList.remove("yellow-letter");
+  pic1.style.display = "none";
+  pic2.style.display = "none";
+  pic3.style.display = "block";
+  pic4.style.display = "none";
+  pic5.style.display = "block";
+  pic6.style.display = "none";
+});
+
+graphicButton.addEventListener("click", () => {
+  graphicButton.classList.add("yellow-letter");
+  showAllButton.classList.remove("yellow-letter");
+  brandingButton.classList.remove("yellow-letter");
+  natureButton.classList.remove("yellow-letter");
+  animationButton.classList.remove("yellow-letter");
+  pic1.style.display = "none";
+  pic2.style.display = "none";
+  pic3.style.display = "none";
+  pic4.style.display = "block";
+  pic5.style.display = "none";
+  pic6.style.display = "block";
+});
+
+natureButton.addEventListener("click", () => {
+  natureButton.classList.add("yellow-letter");
+  showAllButton.classList.remove("yellow-letter");
+  brandingButton.classList.remove("yellow-letter");
+  graphicButton.classList.remove("yellow-letter");
+  animationButton.classList.remove("yellow-letter");
+  pic1.style.display = "block";
+  pic2.style.display = "none";
+  pic3.style.display = "block";
+  pic4.style.display = "none";
+  pic5.style.display = "none";
+  pic6.style.display = "none";
+});
+
+animationButton.addEventListener("click", () => {
+  animationButton.classList.add("yellow-letter");
+  showAllButton.classList.remove("yellow-letter");
+  brandingButton.classList.remove("yellow-letter");
+  graphicButton.classList.remove("yellow-letter");
+  natureButton.classList.remove("yellow-letter");
+  pic1.style.display = "none";
+  pic2.style.display = "block";
+  pic3.style.display = "none";
+  pic4.style.display = "none";
+  pic5.style.display = "none";
+  pic6.style.display = "block";
+});
